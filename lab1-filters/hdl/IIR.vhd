df77d32feb5ad1f50c3b50ff6a1c76674a5de516
Simone Ruffini <simone.ruffini.work@gmail.com>
2023-10-20 13:07:10 +0200

    fix: IIR syntax

--- lab1-filters/hdl/IIR.vhd
+++ lab1-filters/hdl/IIR.vhd
@@ -2,9 +2,9 @@
 -- Engineer: Simone Ruffini [simone.ruffini@studenti.polito.it,
 --                           simone.ruffini@tutanota.com]
 --
--- Create Date:  
+-- Create Date:
 -- Module Name:   IIR
--- Project Name:  
+-- Project Name:
 -- Description:   Infinite Impulse Response Filter first order
 --
 -- Revision:
@@ -18,94 +18,117 @@ library ieee;
   use ieee.numeric_std.all;
 
 library work;
-  use work.IIR_PKG.all;
+  use work.iir_pkg.all;
 ------------------------------------------------------------- ENTITY
 
 entity IIR is
-    generic (
-      A     : sig_arr_type (IIR_ORDER + 1 -1 downto 0 )(IIR_DATA_W -1 downto 0);
-      B     : sig_arr_type (IIR_ORDER + 1 -1 downto 0 )(IIR_DATA_W -1 downto 0);
-      SHAMT : integer;
-    );
-    port (
-      CLK       : in    std_logic;                             -- Clock Signal (rising-edge trigger)
-      RST_AN    : in    std_logic;                             -- Reset Signal: Asyncronous Active Low (Negative)
-      X_in      : in    signed(IIR_DATA_W -1 downto 0);        -- Input sample                     
-      Y_out     : out   signed(IIR_DATA_W -1 downto 0);        -- Output sample
-    );
-  end entity IIR;
- 
-architecture Behavioural of IIR is
-  signal sn             :  signed( IIR_DATA_W downto 0);
-  signal sn_min_1       :  signed( IIR_DATA_W downto 0);
-  signal sn_min_1_tmp   :  std_logic_vector( IIR_DATA_W downto 0);
-  signal s_X            :  signed( IIR_DATA_W - 1 downto 0);
-  signal s_Y            :  signed( IIR_DATA_W - 1 downto 0);
-  signal s_fb           :  signed( IIR_DATA_W downto 0);
-  signal s_ff           :  signed( IIR_DATA_W downto 0);
-  signal s_wb           :  signed( IIR_DATA_W downto 0);
+  generic (
+    A         : sig_arr_type (IIR_ORDER + 1 - 1 downto 0)(IIR_DATA_W - 1 downto 0);
+    B         : sig_arr_type (IIR_ORDER + 1 - 1 downto 0)(IIR_DATA_W - 1 downto 0);
+    SHAMT     : integer
+  );
+  port (
+    CLK       : in    std_logic;                                -- Clock Signal (rising-edge trigger)
+    RST_AN    : in    std_logic;                                -- Reset Signal: Asyncronous Active Low (Negative)
+    X         : in    signed(IIR_DATA_W - 1 downto 0);          -- Input sample
+    Y         : out   signed(IIR_DATA_W - 1 downto 0)           -- Output sample
+  );
+end entity IIR;
 
+architecture BEHAVIOURAL of IIR is
 
-  
+  signal sn             : signed(IIR_DATA_W downto 0);
+  signal sn_min_1       : signed(IIR_DATA_W downto 0);
+  signal sn_min_1_tmp   : std_logic_vector(IIR_DATA_W downto 0);
+  signal s_x            : signed(IIR_DATA_W - 1 downto 0);
+  signal s_y            : signed(IIR_DATA_W - 1 downto 0);
+  signal s_fb           : signed(IIR_DATA_W downto 0);
+  signal s_ff           : signed(IIR_DATA_W downto 0);
+  signal s_wb           : signed(IIR_DATA_W downto 0);
+--
+-- +---+    +---+   fb  +---+   +----+
+-- | X +--->| + |<------+ * |<--+ A1 |
+-- +---+    +-+-+       +---+   +----+
+--            |   +----+  ^
+--            |   | Sx |  |
+--            |   | R  |  |
+--         sn +-->| E  +--+ sn_min_1
+--            |   | G  |  |
+--            |   +----+  |
+--            v           v
+-- +----+   +---+       +---+   +----+
+-- | B0 +-->| * |       | * |<--+ B1 |
+-- +----+   +-+-+       +---+   +----+
+--            | s_wb      v ff
+--            |         +---+    +---+
+--            +-------->| + +--->| Y |
+--                      +---+    +---+
+
 begin
-  
-  In_REG : entity work.REG_PIPO(BEHAV_WITH_EN) 
-  generic map(
-    DATA_W       => ((2*(IIR_DATA_W - SHAMT))-1 downto 0),
-    INIT_VAL     => (others => '0'),
-    RST_INIT_VAL => (others => '0')
-  )
-  port map(
-    CLK   =>        CLK,    
-    RST_A =>        RST_AN,
-    
-    EN    =>        '1',
-    INIT  =>        '0',
-    DIN   =>        X_in,
-    DOUT  =>        s_X
-  );
+
+  IN_REG : entity work.reg_pipo(BEHAV_WITH_EN)
+    generic map (
+      DATA_W       => ((2 * (IIR_DATA_W - SHAMT)) - 1 downto 0),
+      INIT_VAL     => (others => '0'),
+      RST_INIT_VAL => (others => '0')
+    )
+    port map (
+      CLK   => CLK,
+      RST_A => RST_AN,
+
+      EN   => '1',
+      INIT => '0',
+      DIN  => X,
+      DOUT => s_x
+    );
+
   sn_min_1 <= signed(sn_min_1_tmp);
 
-  Out_REG : entity work.REG_PIPO(BEHAV_WITH_EN) 
-  generic map(
-    DATA_W       => ((2*(IIR_DATA_W - SHAMT))-1 downto 0),
-    INIT_VAL     => (others => '0'),
-    RST_INIT_VAL => (others => '0')
-  )
-  port map(
-    CLK   =>        CLK,    
-    RST_A =>        RST_AN,
-    
-    EN    =>        '1',
-    INIT  =>        '0',
-    DIN   =>        std_logic_vector(sn),
-    DOUT  =>        sn_min_1_tmp
-  );
+  OUT_REG : entity work.reg_pipo(BEHAV_WITH_EN)
+    generic map (
+      DATA_W       => ((2 * (IIR_DATA_W - SHAMT)) - 1 downto 0),
+      INIT_VAL     => (others => '0'),
+      RST_INIT_VAL => (others => '0')
+    )
+    port map (
+      CLK   => CLK,
+      RST_A => RST_AN,
+
+      EN   => '1',
+      INIT => '0',
+      DIN  => std_logic_vector(sn),
+      DOUT => sn_min_1_tmp
+    );
+
   sn_min_1 <= signed(sn_min_1_tmp);
 
-  U_Sx_REG : entity work.REG_PIPO(BEHAV_WITH_EN) 
-  generic map(
-    DATA_W       => ((2*(IIR_DATA_W - SHAMT))-1 downto 0),
-    INIT_VAL     => (others => '0'),
-    RST_INIT_VAL => (others => '0')
-  )
-  port map(
-    CLK   =>        CLK,    
-    RST_A =>        RST_AN,
-    
-    EN    =>        '1',
-    INIT  =>        '0',
-    DIN   =>        std_logic_vector(sn),
-    DOUT  =>        sn_min_1_tmp
-  );
+  U_SX_REG : entity work.reg_pipo(BEHAV_WITH_EN)
+    generic map (
+      DATA_W       => ((2 * (IIR_DATA_W - SHAMT)) - 1 downto 0),
+      INIT_VAL     => (others => '0'),
+      RST_INIT_VAL => (others => '0')
+    )
+    port map (
+      CLK   => CLK,
+      RST_A => RST_AN,
+
+      EN   => '1',
+      INIT => '0',
+      DIN  => std_logic_vector(sn),
+      DOUT => sn_min_1_tmp
+    );
+
   sn_min_1 <= signed(sn_min_1_tmp);
 
-  fb <= resize(A(1), IIR_DATA_W+1)*sn_min_1;
-  sn <= fb + resize(s_X, IIR_DATA_W+1);
+  fb <= resize(A(1), IIR_DATA_W + 1) * sn_min_1;
+  sn <= fb + resize(s_x, IIR_DATA_W + 1);
 
-  ff <= resize(B(1), IIR_DATA_W+1)*sn_min_1;
-  s_wb <= resize(B(0), IIR_DATA_W+1)*sn;
-  s_Y <= s_wb + ff;
+  ff   <= resize(B(1), IIR_DATA_W + 1) * sn_min_1;
+  s_wb <= resize(B(0), IIR_DATA_W + 1) * sn;
+  s_y  <= s_wb + ff;
 
-end architecture Behavioural; 
-  ------------------------------------------------------------- ARCHITECTURE: BEHAV_WITH_EN
+  Y <= s_y;
+
+end architecture BEHAVIOURAL;
+
+------------------------------------------------------------- ARCHITECTURE: BEHAV_WITH_EN
