7695dd3f4fd28b09b97fc4194d2187daf8861b2e
Marco Crisologo <s305673@studenti.polito.it>
2023-10-27 16:42:15 +0200

    feat(IIR): change registers, add coefficients registers, compile

--- lab1-filters/hdl/clk_gen.vhd
+++ lab1-filters/hdl/clk_gen.vhd
@@ -3,35 +3,33 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
+
+
 entity clk_gen is
-  port (
-    END_SIM : in  std_logic;
-    CLK     : out std_logic;
-    RST_n   : out std_logic);
+	port (
+		END_SIM:	in  std_logic;
+		CLK:		out std_logic;
+		RST_n:		out std_logic
+	);
 end clk_gen;
+
+
 
 architecture beh of clk_gen is
 
-  constant Ts : time := 100 ns;
-  
-  signal CLK_i : std_logic := '1';
-  
+	constant Ts : time := 100 ns;
+	signal CLK_i : std_logic := '1';
+
 begin  -- beh
 
-  process
-  begin  -- process
-    CLK_i <= not(CLK_i);
-    wait for Ts/2;
-  end process;
+	RST_n <= '0', '1' after 5*Ts/2;
 
-  CLK <= CLK_i and not(END_SIM);
+	process
+	begin  -- process
+		CLK_i <= not(CLK_i);
+		wait for Ts/2;
+	end process;
 
-  process
-  begin  -- process
-    RST_n <= '0';
-    wait for 5*Ts/2;
-    RST_n <= '1';
-    wait;
-  end process;
+	CLK <= CLK_i and not(END_SIM);
 
 end beh;
