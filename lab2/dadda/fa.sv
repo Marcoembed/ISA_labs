3a42e320ebc78280a14fbee7512fcc44547c6a08
Marco Crisologo <s305673@studenti.polito.it>
2023-11-24 22:13:46 +0100

    feat(dadda): fa & ha module

---
+++ lab2/dadda/fa.sv
@@ -0,0 +1,12 @@
+// -------------------- FULL ADDER 
+
+module fa(a,b,cin,sum,cout);
+
+	input a,b,cin;
+	output sum,cout;
+
+   	assign sum  = a ^ b ^ cin;	
+	assign cout = (a & b) | (cin & (a ^ b)); 
+  
+endmodule : fa
+
