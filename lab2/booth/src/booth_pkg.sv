4fbc1e7d0111ab7913e4a022675776fdfb881218
Marco Crisologo <s305673@studenti.polito.it>
2023-12-02 13:57:22 +0100

    feat(src): dadda implementation

---
+++ lab2/booth/src/booth_pkg.sv
@@ -0,0 +1,6 @@
+package booth_pkg;
+		parameter pp_width 	= 11; 	//partial product number of bits
+		parameter pp_deep	= 6;  	//number of partial prodcuts
+		parameter level		= 2;  	//number of iteration levels
+endpackage
+	
---
+++ lab2/booth/src/fa.sv
@@ -0,0 +1,16 @@
+/*------------------------------ FULL ADDER*/
+
+module fa(a,b,cin,sum,cout);
+
+	/*------------------------------ PORT*/
+	input logic a,b,cin;
+	output logic sum,cout;
+
+	/*------------------------------ ALWAYS*/
+	always_comb begin
+   		sum  = a ^ b ^ cin;
+		cout = (a & b) ^ (cin & (a ^ b)); 
+	end
+  
+endmodule : fa
+
