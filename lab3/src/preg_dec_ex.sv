ee359c2228247f126a8dd5e117b0790d75bc5e5e
Marco Crisologo <marcocris99@gmail.com>
2024-01-31 22:21:09 +0100

    feat(linter): set sv linter and test it

---
+++ lab3/src/preg_dec_ex.sv
@@ -0,0 +1,31 @@
+/*--------------------------------------------------------------------------------*/
+// Engineer: Simone Ruffini 	[simone.ruffini@studenti.polito.it],
+//			 Marco Crisolgo 	[s305673@studenti.polito.it],
+//			 Matteo Lago 		[s319914@studenti.polito.it],
+//			 Renato Belmonte 	[s316792@studenti.polito.it],
+//
+// Module Name: Pipeline Register - DEC/EX
+// Project Name: risc-v 
+// Description: 
+//
+// Additional Comments: 
+/*--------------------------------------------------------------------------------*/
+
+
+module preg_dex_ex import riscv_pkg::*;
+	(
+
+    // control input signals
+	input EX_ctrl EXctrl_in,
+	input MEM_ctrl MEMctrl_in,
+	input DEC_ctrl DECctrl_in, 
+
+	// control output signals
+
+    // data output signals
+
+    // data input signals
+
+);
+    
+endmodule
---
+++ lab3/src/wb.sv
@@ -0,0 +1,46 @@
+/*--------------------------------------------------------------------------------*/
+// Engineer: Simone Ruffini 	[simone.ruffini@studenti.polito.it],
+//			 Marco Crisolgo 	[s305673@studenti.polito.it],
+//			 Matteo Lago 		[s319914@studenti.polito.it],
+//			 Renato Belmonte 	[s316792@studenti.polito.it],
+//
+// Module Name: Write Back Multiplexer
+// Project Name: risc-v 
+// Description: 
+//
+// Additional Comments: 
+/*--------------------------------------------------------------------------------*/
+
+
+module wb import riscv_pkg::*;
+    (
+    // Control input signals
+    input WB_ctrl WBctrl_in
+
+    // Data input signals
+    input [31:0] MEMdata_ALU_in,
+    input [31:0] MEMdata_IMM_in,
+    input [31:0] MEMdata_MEM_in,
+
+    // Data output signals
+    output [31:0] MEMdata_MUX_out
+);
+
+
+
+always_comb begin
+    case (WBctrl_in.SRCtoRF)
+        ALUtoRF: begin
+            MEMdata_MUX_out = MEMdata_ALU_in;
+        end
+
+        MEMtoRF: begin
+            MEMdata_MUX_out = MEMdata_MEM_in;
+        end
+
+        IMMtoRF: begin
+            MEMdata_MUX_out = MEMdata_IMM_in;
+        end
+        
+    endcase
+end
