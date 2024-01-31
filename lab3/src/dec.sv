dcf3a00976ff21486e149a1e73a212e014ec5e9e
Renato Belmonte <73286141+RenatoBelmonte@users.noreply.github.com>
2024-01-31 15:28:44 +0100

    WORK in progress DECODE

--- lab3/src/dec.sv
+++ lab3/src/dec.sv
@@ -12,13 +12,38 @@
 /*--------------------------------------------------------------------------------*/
 
 // TARGET = PC + IMM ESTESO
-//
-//module dec import riscv_pkg::*;
-//#(
-//    input   DEC_ctrl    DECctrl_in,
-//
-//
-//    output EX   
-//);
-//    
-//endmodule
+
+module dec import riscv_pkg::*;
+(
+    input   DEC_ctrl        DECctrl_in,
+    input   logic[31:0]     IDdata_INSTRUCTION_in,
+    input   logic [31:0]    IDdata_PC_in,
+
+
+    output   logic [31:0]   MEMdata_PC_out,
+    output   logic [31:0]   MEMdata_IMM_out,
+    output   logic [31:0]   MEMdata_RS1_out,
+    output   logic [31:0]   MEMdata_RS2_out,
+    
+
+);
+
+    logic [4:0] RS1;
+    logic [4:0] RS2;
+
+    imm_gen imm_gen_unit (
+        .instruction_i(IDdata_INSTRUCTION_in),
+        .immediate_o(MEMdata_IMM_out)
+    );
+
+    bu bu_unit (
+        .DECctrl_in(DECctrl_in),
+        .RS1()
+    );
+
+    always_comb begin
+        
+        
+    end
+    
+endmodule
