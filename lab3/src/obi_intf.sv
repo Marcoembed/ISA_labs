2c422caf0584f49e40391934132bd2be7ca5f87b
Marco Crisologo <marcocris99@gmail.com>
2024-02-08 12:39:29 +0100

    feat(obi_interface): signals obi interface

--- lab3/src/obi_intf.sv
+++ lab3/src/obi_intf.sv
@@ -1,18 +1,15 @@
-interface obi_intf;
+interface obi_intf import riscv_pkg::*;;
+	rdwr we;
+	obi_req proc_req;
     logic mem_rdy;
     logic valid;
-    logic [31:0] addressBus_in;
-    logic [31:0] addressBus_out;
-    logic [31:0] dataBus_in;
+    logic [width-1:0] wdata;
+    logic [width-1:0] rdata;
+    logic [reg_width-1:0] addr;
 
     modport PtoIntf ( // processor to interface
         input mem_rdy, valid, dataBus_in, addressBus_in, // in processor
         output addressBus_out  // from processor
-    );
-
-    modport MtoIntf ( // memory to interface
-        input addressBus_out, // in memory 
-        output mem_rdy, valid, dataBus_in // from memory
     );
 
 endinterface //obi (input CLK)
