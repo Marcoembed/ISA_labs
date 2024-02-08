b4576d3779a51f467d84cd52ae0b1ea3faff675b
Matteshala <s319914@studenti.polito.it>
2024-02-08 12:54:32 +0100

    feat(interface): obi interface

--- lab3/src/obi_intf.sv
+++ lab3/src/obi_intf.sv
@@ -1,15 +1,16 @@
 interface obi_intf import riscv_pkg::*;;
-	rdwr we;
 	obi_req proc_req;
     logic mem_rdy;
-    logic valid;
+	rdwr we;
+    logic [reg_width-1:0] addr;
     logic [width-1:0] wdata;
     logic [width-1:0] rdata;
-    logic [reg_width-1:0] addr;
+    logic valid;
 
-    modport PtoIntf ( // processor to interface
-        input mem_rdy, valid, dataBus_in, addressBus_in, // in processor
-        output addressBus_out  // from processor
+
+    modport to_mem ( // processor to interface
+        output proc_req, we, addr, wdata, // to memory
+        input mem_rdy, valid, rdata  // from memory
     );
 
 endinterface //obi (input CLK)
