98fdf9960e603cf7a36dd51b32a38ecf77bd8449
Renato Belmonte <73286141+RenatoBelmonte@users.noreply.github.com>
2024-02-23 17:17:56 +0100

    refactor(scripts and names)

--- lab3/sim/mem_fake_sim.sh
+++ lab3/sim/mem_fake_sim.sh
@@ -5,14 +5,19 @@ source /eda/scripts/init_questa_core_prime
 rm -rf work
 vlib work
 
+#rm -rf mem_wrap
+#vlib mem_wrap
+
+tres="ps"
+
+#vcom -work mem_wrap -nodebug ../src/mem_wrap_fake2.0.vhd
+
 ls ../src/*.sv | grep -vE 'riscv_top.sv|riscv_pkg.sv' > compile_VLOG.f
-ls ../tb/tb_mem_real.sv >> compile_VLOG.f
+ls ../tb/tb.sv >> compile_VLOG.f
 ls ../tb/*.vhd > compile_VHDL.f
 
 vlog -work work -svinputport=relaxed ../src/riscv_pkg.sv
-vlog -work ./work ../sram_32_1024_freepdk45/sram_32_1024_freepdk45.v
-
-vcom -work work  -F compile_VHDL.f
 vlog -work work -svinputport=relaxed -F compile_VLOG.f
+vcom -work work  -F compile_VHDL.f
 
-vsim  -t ps -do wave_real_mem.do work.tb_mem_real -voptargs=+acc -suppress 3009
+vsim -t ${tres} -L ./mem_wrap -do wave2.do work.tb_fake -voptargs=+acc
--- lab3/sim/mem_real_sim.sh
+++ lab3/sim/mem_real_sim.sh
@@ -14,9 +14,9 @@ vlog -work work -svinputport=relaxed -F compile_VLOG.f
 vlog -work ./work ../netlist/riscv_top.v
 
 vlog -work ./work ../sram_32_1024_freepdk45/sram_32_1024_freepdk45.v
-vlog -work ./work ../tb/tb_post_syn_real.sv
+vlog -work ./work ../tb/tb_real.sv
 
 
 ## NOTE: the memory has no timing annotation, thus sdf simulation is not possible as sdf symbols have no match in the memory verilog model
-vsim  -t ps -L /eda/dk/nangate45/verilog/qsim2020.4 work.tb_post_syn_real -suppress 3009 -do wave_top.do -voptargs=+acc 
+vsim  -t ps -L /eda/dk/nangate45/verilog/qsim2020.4 work.tb_real -suppress 3009 -do wave_top.do -voptargs=+acc 
 
