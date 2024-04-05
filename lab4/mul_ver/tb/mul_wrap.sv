ef1d18c6157c08a7e24cc8e8224de4c28f1e034b
Marco Crisologo <marcocris99@gmail.com>
2024-04-05 16:42:52 +0200

    fix mul wrap position

--- lab4/mul_ver/tb/mul_wrap.sv
+++ lab4/mul_ver/tb/mul_wrap.sv
@@ -1,12 +1,12 @@
 /* Module wrap Multiplier */
 
 module mul_wrap #(parameter DWIDTH = 32) (
-    alu_if.mul_port mp
+    alu_if.alu_port p
 );
     multiplier #(DWIDTH) mul_u (
-        .alu_a_i    (mp.alu_a),
-        .alu_b_i    (mp.alu_b),
-        .alu_res_o  (mp.alu_res)
+        .alu_a_i    (p.alu_a),
+        .alu_b_i    (p.alu_b),
+        .alu_res_o  (p.alu_res)
     );
 endmodule
 
