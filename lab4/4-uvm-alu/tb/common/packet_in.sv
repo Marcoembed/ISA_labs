fff169b493838711c981ff6c70a4e130af1442f7
Renato Belmonte <73286141+RenatoBelmonte@users.noreply.github.com>
2024-04-16 13:33:57 +0200

    bug(sequencer):old packet in

--- lab4/4-uvm-alu/tb/common/packet_in.sv
+++ lab4/4-uvm-alu/tb/common/packet_in.sv
@@ -7,6 +7,8 @@ class packet_in extends uvm_sequence_item;
     `uvm_object_utils_begin(packet_in)
         `uvm_field_int(A, UVM_ALL_ON|UVM_HEX)
         `uvm_field_int(B, UVM_ALL_ON|UVM_HEX)
+        `uvm_field_int(C, UVM_ALL_ON|UVM_HEX)
+        `uvm_field_enum(operation_e, op, UVM_ALL_ON|UVM_HEX)
     `uvm_object_utils_end
 
     function new(string name="packet_in");
